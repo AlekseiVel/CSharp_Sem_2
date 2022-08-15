07156e7a4079215be354d89d2e0dd3b4358adace
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-15 21:59:17 +0300

    hometask3 ver1.0 not working

---
+++ Hometask3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или
+// сообщает, что третьей цифры нет!
+
+int numberA = 0, middle_result = 0, third = -1;
+
+Console.Write("Введите число: ");
+numberA = int.Parse(Console.ReadLine()!);
+
+if(numberA >=100 && numberA <= 999)
+{
+    third = numberA % 10;
+    Console.WriteLine($"Третья цифра числа {numberA}: {third}");
+}
+
+while(middle_result >= 100 && middle_result <= 999)
+{
+    middle_result = numberA / 10;
+}
+third = middle_result % 10;
+Console.WriteLine($"Третья цифра числа {numberA}: {third}");
+if(numberA >= 1 && numberA <= 99)
+{
+    Console.WriteLine($"У числа {numberA} нет второй цифры");
+}

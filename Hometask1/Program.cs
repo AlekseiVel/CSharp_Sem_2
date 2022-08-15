afe3440f1819c52714588b43f5e8f02e550548e9
«AlekseiVel» <velmezov.au@gmail.com>
2022-08-15 21:15:53 +0300

    Hometask1 ver1

---
+++ Hometask1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+
+int number = 0, first = 0, third = 0;
+
+Console.Write("Введите трехзначное число: ");
+number = int.Parse(Console.ReadLine()!);
+
+first = number / 100;
+third = number % 10;
+
+Console.WriteLine($"Вторая цифра удалена, результат: {first}{third}");
